83bf110b9f55b93a569cfe66cdf295c264af8fe0
Mehrab <mehrabgheibi@gmail.com>
2023-04-13 01:32:48 +0430

    module.scss is implemented for design

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,25 +1,26 @@
 import { Fragment } from "react";
-import "../../styles/common/header.scss";
+
+import classes from "../../styles/common/header.module.scss";
 
 const Header = () => {
   return (
     <Fragment>
-      <div className="header"></div>
-      <div className="header_text">
-        <h1 className="first_title"> Main Name </h1>
-        <h2 className="second_title"> Construction Company </h2>
-        <p className="address">
+      <div className={classes.header}></div>
+      <div className={classes.header_text}>
+        <h1 className={classes.first_title}> Main Name </h1>
+        <h2 className={classes.second_title}> Construction Company </h2>
+        <p className={classes.address}>
           West unit, Third floor, No.17, 2nd st., Navvab Ave,
           <br /> TEHRAN, IRAN
         </p>
-        <p className="address"> P.O. Box: 1234567891 </p>
-        <div className="links">
+        <p className={classes.address}> P.O. Box: 1234567891 </p>
+        <div className={classes.links}>
           <a href=""> (+98) 21 12345678 </a>
           <br />
           <a href=""> example@gmail.com </a>
         </div>
       </div>
-      <div className="diagonal_shadow"></div>
+      <div className={classes.diagonal_shadow}></div>
     </Fragment>
   );
 };
