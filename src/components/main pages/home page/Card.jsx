6cb8f46d6fd01c249be020d03012f9c1c1fcfd83
Mehrab <mehrabgheibi@gmail.com>
2023-05-14 12:36:43 +0430

    projects section is implemented

---
+++ src/components/main pages/home page/Card.jsx
@@ -0,0 +1,18 @@
+import classes from "../../../styles/main pages/home page/projects.module.scss";
+import ArrowIcon from "../../../images/home page/arrow.svg";
+
+const Card = ({ imgSrc, title, content, onClick }) => {
+  return (
+    <div className={classes.card_wrapper} onClick={onClick}>
+      <img className={classes.image} src={imgSrc} alt={title} />
+
+      <div className={classes.lower_of_card}>
+        <h1 className={classes.title}>{title}</h1>
+        <img className={classes.arrow_icon} src={ArrowIcon} alt="" />
+        <p className={classes.content}>{content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/main pages/home page/Projects_Desktop.jsx
+++ src/components/main pages/home page/Projects_Desktop.jsx
@@ -1,5 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Card from "./Card";
+import companyProjects from "../../../data information/companyProjects";
+
+import classes from "../../../styles/main pages/home page/projects.module.scss";
+import leftArrow from "../../../images/home page/projects/left-arrow.svg";
+import rightArrow from "../../../images/home page/projects/right-arrow.svg";
+
 const Projects_Desktop = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  const renderSlides = () => {
+    return companyProjects.map((cardInfo) => (
+      <Card
+        key={cardInfo.id}
+        imgSrc={cardInfo.image}
+        title={cardInfo.title}
+        content={cardInfo.shortContent}
+        onClick={() => navigate(cardInfo.link)}
+      />
+    ));
+  };
+
+  const arrow = (src) => {
+    return <img src={src} alt="" />;
+  };
+
+  return (
+    <div className={classes.projects}>
+      <h2 className={classes.projects_title}>LATEST PROJECTS</h2>
+      <Slider
+        dots={false}
+        slidesToShow={4}
+        slidesToScroll={1}
+        autoplay={true}
+        autoplaySpeed={3000}
+        prevArrow={arrow(leftArrow)}
+        nextArrow={arrow(rightArrow)}
+      >
+        {renderSlides()}
+      </Slider>
+    </div>
+  );
 };
 
 export default Projects_Desktop;
