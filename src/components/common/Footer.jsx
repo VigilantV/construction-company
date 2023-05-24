a0e040296ffacd0ffba3c4d18b29b534525badc2
Mehrab <mehrabgheibi@gmail.com>
2023-05-24 10:18:38 +0330

    footer is implemented

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,73 @@
+import { useState } from "react";
+
+import info from "../../data information/companyInformations";
+import socialIcons from "../../data information/socialIcons";
+
+import classes from "../../styles/common/footer.module.scss";
+import logo from "../../images/main_logo.svg";
+
 const Footer = () => {
-  return <div></div>;
+  const [emailValue, setEmailValue] = useState("");
+  const [textAreaValue, setTextAreaValue] = useState("");
+
+  const companyInfo = (
+    <>
+      <h2 className={classes.second_title}>{info.secondTitle}</h2>
+      <p className={classes.address}>{info.address}</p>
+      <p className={classes.address}>{info.PO_Box}</p>
+      <div className={classes.links}>
+        <p>{info.number}</p>
+        <br />
+        <p>{info.email}</p>
+      </div>
+    </>
+  );
+
+  return (
+    <div className={classes.footer}>
+      <div>
+        <img src={logo} className={classes.logo} alt="logo" />
+        <div>{companyInfo}</div>
+        <div className={classes.link_icons}>
+          {socialIcons.map((icon) => (
+            <a key={icon.id} href={icon.link}>
+              <img className={classes.icon} src={icon.src} alt={icon.alt} />
+            </a>
+          ))}
+        </div>
+      </div>
+      <div>
+        <h1 className={classes.input_title}>Get In Touch</h1>
+        <form
+          className={classes.form}
+          onSubmit={(e) => {
+            e.preventDefault();
+            setEmailValue("");
+            setTextAreaValue("");
+          }}
+        >
+          <input
+            className={classes.input_elements}
+            type="email"
+            placeholder="Email"
+            value={emailValue}
+            onChange={(e) => {
+              setEmailValue(e.target.value);
+            }}
+          />
+          <textarea
+            className={classes.input_elements}
+            placeholder="Message"
+            value={textAreaValue}
+            onChange={(e) => setTextAreaValue(e.target.value)}
+          />
+          <button className={classes.btn} type="submit">
+            submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
---
+++ src/data information/socialIcons.js
@@ -0,0 +1,47 @@
+import facebook from "../images/footer/icons/facebook.svg";
+import google from "../images/footer/icons/google.svg";
+import instagram from "../images/footer/icons/instagram.svg";
+import linkedin from "../images/footer/icons/linkedin.svg";
+import telegram from "../images/footer/icons/telegram.svg";
+import twitter from "../images/footer/icons/twitter.svg";
+
+const socialIcons = [
+  {
+    id: 0,
+    src: facebook,
+    alt: "facebook",
+    link: "",
+  },
+  {
+    id: 1,
+    src: google,
+    alt: "google",
+    link: "",
+  },
+  {
+    id: 2,
+    src: instagram,
+    alt: "instagram",
+    link: "",
+  },
+  {
+    id: 3,
+    src: linkedin,
+    alt: "linkedin",
+    link: "",
+  },
+  {
+    id: 4,
+    src: telegram,
+    alt: "telegram",
+    link: "",
+  },
+  {
+    id: 5,
+    src: twitter,
+    alt: "twitter",
+    link: "",
+  },
+];
+
+export default socialIcons;
