851324cdad7c6c574acb341a34be912c95135e26
Mehrab <mehrabgheibi@gmail.com>
2023-05-24 10:18:20 +0330

    header data is transfered to js file

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,27 +1,24 @@
-import { Fragment } from "react";
+import info from "../../data information/companyInformations";
 
 import classes from "../../styles/common/header.module.scss";
 
 const Header = () => {
   return (
-    <Fragment>
+    <>
       <div className={classes.header}></div>
       <div className={classes.header_text}>
-        <h1 className={classes.first_title}> Main Name </h1>
-        <h2 className={classes.second_title}> Construction Company </h2>
-        <p className={classes.address}>
-          West unit, Third floor, No.17, 2nd st., Navvab Ave,
-          <br /> TEHRAN, IRAN
-        </p>
-        <p className={classes.address}> P.O. Box: 1234567891 </p>
+        <h1 className={classes.first_title}>{info.firstTitle}</h1>
+        <h2 className={classes.second_title}>{info.secondTitle}</h2>
+        <p className={classes.address}>{info.address}</p>
+        <p className={classes.address}>{info.PO_Box}</p>
         <div className={classes.links}>
-          <p> (+98) 21 12345678 </p>
+          <p>{info.number}</p>
           <br />
-          <p> example@gmail.com </p>
+          <p>{info.email}</p>
         </div>
       </div>
       <div className={classes.diagonal_shadow}></div>
-    </Fragment>
+    </>
   );
 };
 
---
+++ src/data information/companyInformations.js
@@ -0,0 +1,14 @@
+const companyInformations = {
+  firstTitle: "Main Name",
+  secondTitle: "Construction Company",
+  address: (
+    <>
+      West unit, Third floor, No.17, 2nd st., Navvab Ave, <br /> TEHRAN, IRAN"
+    </>
+  ),
+  PO_Box: "P.O. Box: 1234567891",
+  number: "(+98) 21 12345678",
+  email: "example@gmail.com",
+};
+
+export default companyInformations;
