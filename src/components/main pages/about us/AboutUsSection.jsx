bd8f0fcb467a2c1268ef955b9a12755bcd1d9322
Mehrab <mehrabgheibi@gmail.com>
2023-06-09 15:15:33 +0330

    mobile version of aboutUsSection is implemented

--- src/components/main pages/about us/AboutUsSection.jsx
+++ src/components/main pages/about us/AboutUsSection.jsx
@@ -8,8 +8,8 @@ const AboutUsSection = () => {
       <h2 className={classes.title}>ABOUT US</h2>
       {aboutUsArticles.map((article) => (
         <div key={article.title} className={classes.article}>
-          <div className={classes.left_side}>{article.title}</div>
-          <div className={classes.right_side}>{article.content}</div>
+          <div className={classes.article_title}>{article.title}</div>
+          <div className={classes.article_text}>{article.content}</div>
         </div>
       ))}
     </div>
